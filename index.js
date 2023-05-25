7523d08b41b0dc763322e254991066b18ffcfbff
JUN <xojun412@gmail.com>
2023-05-25 13:26:13 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,13 +1,66 @@
 const express = require('express')
 const app = express()
 const port = 4000
+const bodyParser = require('body-parser');
+const {User} = require("./Users.js");
+
+const config = require("./config/key");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jun:jun102030!@cluster0.0syfwcw.mongodb.net/?retryWrites=true&w=majority',{
-    useNewUrlParser: true, useUnifiedTopology: true
-}).then(() => console.log('MongoDB Connected...'))
-.catch(err => console.log(err))
+mongoose.connect(config.mongoURI,{
+     useNewUrlParser: true, useUnifiedTopology: true
+ }).then(() => console.log('MongoDB Connected...'))
+ .catch(err => console.log(err))
 
-app.get('/',(req,res) => res.send('Hello World!ddddd'))
+// app.post('/register', async (req,res) => {
+
+//     const user = new User(req.body)
+
+//     await user
+//     .save()
+//     .then(() => {
+//         res.status(200).json({
+//             success: true,
+//         });
+//     })
+//     .catch((err) => {
+//         console.error(err);
+//         res.json({
+//             success: false,
+//             err: err,
+//         })
+//     })
+// })
+
+app.get('/',(req,res) => res.send('ddddd'))
+
+app.post('/register', async(req,res) => {
+    
+    //회원 가입할 때  필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다. 
+
+    const user = new User(req.body)
+
+    await user
+    .save()
+    .then(() => {
+        res.status(200).json({
+            success: true,
+        });
+    })
+    .catch((err) => {
+        console.error(err);
+        res.json({
+            success: false,
+            err: err,
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on part ${port}!`))
